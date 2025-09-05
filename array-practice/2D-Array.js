73a4d08ca549dd75542a4345b9c662df2847d5fc
hasanaat <arsheema619@gmail.com>
2025-09-05 13:26:54 +0530

    filtered items

--- array-practice/2D-Array.js
+++ array-practice/2D-Array.js
@@ -1,6 +1,9 @@
+// 2D array = multi-dimensional array that store a matrix of data in rows and columns.
+
 const matrix = [[1,2,3],
               [4,5,6],
               [7,8,9]];
 for (let row of matrix){
-    console.log(row);
+    let rowString = row.join(' ');
+    console.log(rowString);
 }
